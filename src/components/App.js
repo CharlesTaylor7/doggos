c026a06cceb4a48bfcde9c4ef79e064e4121dae5
Charles Taylor <charlest@paragontruss.com>
2019-08-04 22:40:40 -0400

    Fetch doggos from giphy

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react';
 import doggo from '../assets/doggo.jpg'
 import './App.css';
 import { useFrames } from '../hooks/useFrames';
+import { doggo$ } from '../sources/giphy';
 
 export const App = () => {
   const { frame, forward, backward, append } = useFrames();
+
+  useEffect(() => {
+    doggo$.subscribe(doggo => append(doggo));
+  }, [doggo$]);
 
   useEffect(() => {
     console.log(frame);
@@ -14,13 +19,13 @@ export const App = () => {
     document.onkeydown = (e) => {
       if (e.key === 'ArrowLeft') backward();
       else if (e.key === 'ArrowRight') forward();
-      else append(e.key);
+      // else append(e.key);
     }
-  }, [forward, backward, append]);
+  }, [forward, backward]);
 
   return (
     <div className="App">
-      <img src={doggo} className="App-logo" alt="logo" />
+      <img src={frame} className="App-logo" alt="logo" />
     </div>
   );
 }
